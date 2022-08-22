6306353f2d66e3c44d2be97a8e12effb04c191a2
DmitryB <BogomolovDME@yandex.ru>
2022-08-22 20:48:21 +0300

    commit

---
+++ Task001/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// "Напишите ЦИКЛ" значит возводить в степень нужно циклом, а не методом Math.Pow
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("Программа для возведения чисел в степень");
+// Console.WriteLine("Введите число ");
+// string? numberString1 = Console.ReadLine();
+// int a = int.Parse(numberString1!);
+
+// Console.WriteLine("Введите степень, в которую нужно ввести число");
+// string? numberString2 = Console.ReadLine();
+// int b = int.Parse(numberString2!);
+
+// int degree = a;
+
+// for (int i = 1; i < b; i++)
+// {
+//     degree = degree * a;
+// }
+// Console.WriteLine("Результат возведения в степень =" + degree);
+
+Console.WriteLine("Программа для возведения чисел в степень");
+int Degree(int numberA, int numberB)
+{
+    int summ = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        summ = summ * numberA;
+    }
+    return summ;
+}
+
+Console.WriteLine("Введите число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень, в которую нужно ввести число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int degree = Degree(numberA, numberB);
+Console.WriteLine("Результат возведения в степень =" + degree);
---
+++ Task002/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+/* Console.WriteLine("Программа для вычисления суммы цифр в числе");
+
+Console.WriteLine("Введите число ");
+string? numberString1 = Console.ReadLine();
+int a = int.Parse(numberString1!);
+int sum = 0;
+if (a < 0) //Небольшая защита от "дурака"
+{
+    a = a * (-1);
+}
+
+while (a > 0)
+{
+    int num = a % 10;
+    a = a / 10;
+    sum = sum + num;
+}
+Console.WriteLine("Сумма цифр в числе =" + sum); */
+
+Console.WriteLine("Программа для вычисления суммы цифр в числе");
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNum(int number)
+{
+    int count = Convert.ToString(number).Length;
+    int step = 0;
+    int sum = 0;
+    if (number < 0)
+    {
+        number = number * (-1);
+    }
+    for (int i = 0; i < count; i++)
+    {
+        step = number % 10;
+        sum = sum + step;
+        number = number / 10;
+    }
+    return sum;
+}
+
+int sumNum = SumNum(number);
+Console.WriteLine("Сумма цифр в числе: " + sumNum);
+
